9e122146c71dfd2988a56160d7a7f07de755a450
Arella <arellamirasol005@gmail.com>
2025-11-22 19:53:41 +0800

    added memory game button on Home

--- 4games/src/Pages/Home.jsx
+++ 4games/src/Pages/Home.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import { useState } from 'react';
 import './Auth.css';
 
 const Home = () => {
+	const [showComponent, setShowComponent] = useState(null);
+
 	const user = JSON.parse(localStorage.getItem('user') || 'null');
 	return (
 		<div style={{ padding: '2rem', color: '#fff' }}>
 			<h1>Welcome{user ? `, ${user.username}` : ''}!</h1>
 			<p>This is the Home page. Your frontend is running.</p>
+			<button onClick={()=> setShowComponent('memorygame')}>Memory Game</button>
+
+			<div>
+				{showComponent === 'memorygame' && <Memorygame />}
+			</div>
 		</div>
 	);
 };
