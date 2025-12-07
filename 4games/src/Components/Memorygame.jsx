3ae091b15262ea8bdcae29cc4cdcf65e0f634773
Arella <arellamirasol005@gmail.com>
2025-12-07 23:07:08 +0800

    WE'RE GETTING SOMEWHERE RAAAAAAAAAAAA

--- 4games/src/Components/Memorygame.jsx
+++ 4games/src/Components/Memorygame.jsx
@@ -1,48 +1,61 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './Memorygame.css';
 
 const INITIAL_CARDCOUNT = 4;
-const CARDS = [
-
-]
 const STEP = 2;
 
 const Memorygame = () => {
     const navigate = useNavigate();
-    const [leaderboard, setLeaderboard] = useState(() =>{
-        const save = localStorage.getItem('mgleaderboard');
-        return save ? JSON.parse(save) : [];
-    });
-    const [cardcount, setCardcount] = useState(INITIAL_CARDCOUNT)
+    const [cardCount, setCardcount] = useState(INITIAL_CARDCOUNT);
 
     return (
-        <div className="container">
-            <div className="leftside">
+        <div className='contain'>
+            <div className='top'>
+                <h2> Super cutesy memory game! </h2>
+                <p> to test your super cutesy memory!</p>
+            </div>
+
+            <div className='leftside'>
+                <div className='howto'>
+                    <h2>How to play the game:</h2>
+                    <p>1. Click on the cards to flip them.</p>
+                    <p>2. Try and find the matching pairs.</p>
+                    <p>3. Aim to get the least amount of turns.</p>
+                    <p>4. Pray and hope you aren't showing early signs of dementia.</p>
+
+                    <button onClick={() => navigate('/home')} style={{marginTop: '23rem'}}> Back to Home </button>
+                </div>
 
             </div>
 
-            <div className="center">
+            <div className='center'>
+                <h1>Amount of cards:</h1>
+                <Counter cardsCount={cardCount} onClick={setCardcount} />
+                <button > Start Game </button>
+            </div>
+
+            <div className='rightside'>
 
             </div>
 
-            <div className="rightside">
+            <div className='bottom'>
 
             </div>
-
         </div>
     );
 };
 
-const Counter = ({ cardsCount, onClick }) => {
+const Counter = ({ cardCount, onClick }) => {
     const onDecrement = e => {
         e.preventDefault();
-        const number = cardsCount - STEP;
+        const number = cardCount - STEP;
         if (number >= 2) onClick(number);
     };
 
     const onIncrement = e => {
         e.preventDefault();
-        const number = cardsCount + STEP;
+        const number = cardCount + STEP;
         if (number <= 160) onClick(number);
     };
 
@@ -51,7 +64,7 @@ const Counter = ({ cardsCount, onClick }) => {
             <button className="minus" onClick={onDecrement}>
                 -
             </button>
-            <span className="quantity">{cardsCount}</span>
+            <span className="quantity">{cardCount}</span>
             <button className="plus" onClick={onIncrement}>
                 +
             </button>
